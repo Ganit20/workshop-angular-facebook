45b99515106f13c63b711c78636a33d8e8be4aca
Bartlomiej Sulek <bsulek@mobiltek.pl>
2022-06-09 09:50:25 +0200

    Fixtures

--- src/app/core/components/pages/home-page/home-page.component.ts
+++ src/app/core/components/pages/home-page/home-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import fixturePosts from '../../../../../fixtures/posts.fixture.json'
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
+  fakePosts = fixturePosts;
   constructor() { }
 
   ngOnInit(): void {
